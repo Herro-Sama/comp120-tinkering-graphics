53d801bfaeba9c846e4efb66953984de65237f9f
lstent <lucstent@gmail.com>
2016-11-08 17:43:16 +0000

    Goes dark when you press down

--- Images/Biodome.py
+++ Images/Biodome.py
@@ -1,8 +1,5 @@
-import pygame, sys, time
-
-
-#Colour
-white = (255,255,255)
+import pygame, sys
+from pygame.locals import *
 
 #Create window
 window_width = 640
@@ -11,13 +8,28 @@ screen = pygame.display.set_mode([window_width, window_height])
 
 #Background
 Background = pygame.image.load('Biodome.jpg')
-screen.blit(Background,[0, 0])
+
+
+def Bbolt(screen):
+    for Y in xrange(window_height):
+        for X in xrange(window_width):
+            Red = screen.get_at((X, Y)).r
+            Green = screen.get_at((X, Y)).g
+            Blue = screen.get_at((X, Y)).b
+            pxarray[X, Y] = (Red/4, Green/4, Blue/4)
 
 pygame.init
 
-pygame.display.update()
-
+screen.blit(Background,[0, 0])
 while True:
+    keys_pressed = pygame.key.get_pressed()
+    pxarray = pygame.PixelArray(screen)
+
+    if keys_pressed[K_DOWN]:
+        Bbolt(Background)
+
+    del pxarray
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type ==pygame.QUIT :
             sys.exit()
