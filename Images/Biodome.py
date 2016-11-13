c739815b6e63b3d710cd8cd54b25240db09c46e1
lstent <lucstent@gmail.com>
2016-11-13 17:09:10 +0000

    Sunset maybe

--- Images/Biodome.py
+++ Images/Biodome.py
@@ -1,35 +1,35 @@
-import pygame, sys
+import pygame
+import sys
+import time
 from pygame.locals import *
 
-#Create window
+# Colours
+Red = (255, 0, 0)
+Green = (0, 255, 0)
+Blue = (0, 0, 255)
+
+# Create window
 window_width = 640
 window_height = 350
 screen = pygame.display.set_mode([window_width, window_height])
 
-#Background
-Background = pygame.image.load('Biodome.jpg')
-
-
-def Bbolt(screen):
-    for Y in xrange(window_height):
-        for X in xrange(window_width):
-            Red = screen.get_at((X, Y)).r
-            Green = screen.get_at((X, Y)).g
-            Blue = screen.get_at((X, Y)).b
-            pxarray[X, Y] = (Red/4, Green/4, Blue/4)
+# Define Sunset Function, background & increase pixel colour 4 times
+def Sunset():
+    Background = pygame.image.load('Biodome.jpg')
+    screen.blit(Background,[0, 0])
+    pxarray = pygame.PixelArray(Background)
+    for i in range(0, 4):
+        for Y in xrange(window_height):
+            for X in xrange(window_width):
+                Red = screen.get_at((X, Y)).r
+                Green = screen.get_at((X, Y)).g
+                Blue = screen.get_at((X, Y)).b
+                pxarray[X, Y] = (Red*0.8, Green*0.7, Blue*0.7)
+                pygame.display.update()
 
 pygame.init
-
-screen.blit(Background,[0, 0])
 while True:
-    keys_pressed = pygame.key.get_pressed()
-    pxarray = pygame.PixelArray(screen)
-
-    if keys_pressed[K_DOWN]:
-        Bbolt(Background)
-
-    del pxarray
-    pygame.display.update()
+    Sunset()
     for event in pygame.event.get():
-        if event.type ==pygame.QUIT :
+        if event.type == pygame.QUIT :
             sys.exit()
